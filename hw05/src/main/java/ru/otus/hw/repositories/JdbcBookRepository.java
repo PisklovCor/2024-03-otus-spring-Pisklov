b75666e5931472c2e519426449cd200fee5880fd
Andrey <andreypisklov@gmail.com>
2024-05-05 15:47:38 +0300

    [hw-05]: CRUD книги

--- hw05/src/main/java/ru/otus/hw/repositories/JdbcBookRepository.java
+++ hw05/src/main/java/ru/otus/hw/repositories/JdbcBookRepository.java
@@ -5,9 +5,12 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
+import ru.otus.hw.exceptions.EntityNotFoundException;
 import ru.otus.hw.models.Author;
 import ru.otus.hw.models.Book;
 import ru.otus.hw.models.Genre;
@@ -89,21 +92,32 @@ public class JdbcBookRepository implements BookRepository {
 
     }
 
+    @SuppressWarnings("DataFlowIssue")
     private Book insert(Book book) {
         var keyHolder = new GeneratedKeyHolder();
 
-        //...
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("title", book.getTitle());
+        params.addValue("author_id", book.getAuthor().getId());
+        jdbc.update("insert into books (title, author_id) values (:title, :author_id)", params, keyHolder, new String[]{"id"} );
 
-        //noinspection DataFlowIssue
         book.setId(keyHolder.getKeyAs(Long.class));
         batchInsertGenresRelationsFor(book);
         return book;
     }
 
     private Book update(Book book) {
-        //...
 
-        // Выбросить EntityNotFoundException если не обновлено ни одной записи в БД
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id", book.getId());
+        params.addValue("title", book.getTitle());
+        params.addValue("author_id", book.getAuthor().getId());
+        int result = jdbc.update("update books set title = :title, author_id = :author_id where id = :id", params);
+
+        if (result < 1) {
+            throw new EntityNotFoundException("The book is not updated");
+        }
+
         removeGenresRelationsFor(book);
         batchInsertGenresRelationsFor(book);
 
@@ -111,11 +125,18 @@ public class JdbcBookRepository implements BookRepository {
     }
 
     private void batchInsertGenresRelationsFor(Book book) {
-        // Использовать метод batchUpdate
+
+        final long bookId = book.getId();
+        List<Genre> genres = book.getGenres();
+
+        final String query = "insert into books_genres (book_id, genre_id) values (%s, :id)";
+
+        jdbc.batchUpdate(String.format(query, bookId), SqlParameterSourceUtils.createBatch(genres));
     }
 
     private void removeGenresRelationsFor(Book book) {
-        //...
+        Map<String, Object> params = Collections.singletonMap("book_id", book.getId());
+        jdbc.update("delete from books_genres where book_id = :book_id", params);
     }
 
     private static class BookRowMapper implements RowMapper<Book> {
