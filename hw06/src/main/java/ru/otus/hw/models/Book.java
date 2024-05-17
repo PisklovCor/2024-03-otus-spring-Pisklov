6796bb21e0ce9e6dc634bf7ad4cbdb30c08cb4e7
Andrey <andreypisklov@gmail.com>
2024-05-17 19:39:51 +0300

    [hw-06]: драфт сервиса

---
+++ hw06/src/main/java/ru/otus/hw/models/Book.java
@@ -0,0 +1,35 @@
+package ru.otus.hw.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", referencedColumnName = "id")
+    private Author author;
+
+    @Fetch(FetchMode.SELECT)
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "books_genres", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id"))
+    private List<Genre> genres;
+}
