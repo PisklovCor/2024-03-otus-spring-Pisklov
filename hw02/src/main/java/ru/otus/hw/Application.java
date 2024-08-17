3c9ad93c46b8c0fb6893da0c8b338166218e8a69
Andrey <andreypisklov@gmail.com>
2024-08-17 13:47:04 +0300

    [hw-02]: замечания после ревью: перенос анотация в головной класс Application.java

--- hw02/src/main/java/ru/otus/hw/Application.java
+++ hw02/src/main/java/ru/otus/hw/Application.java
@@ -2,9 +2,13 @@ package ru.otus.hw;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import ru.otus.hw.service.TestRunnerServiceImpl;
 
 @ComponentScan
+@Configuration
+@PropertySource("classpath:application.properties")
 public class Application {
     public static void main(String[] args) {
 
