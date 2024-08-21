d8e5ce1eba093639548dbfb0cd90ea1224431304
Andrey <andreypisklov@gmail.com>
2024-08-21 20:30:21 +0300

    [hw-10]: обнволен ControllerAdvice --> RestControllerAdvice

--- hw10/src/main/java/ru/otus/hw/controllers/ExceptionHandlingController.java
+++ hw10/src/main/java/ru/otus/hw/controllers/ExceptionHandlingController.java
@@ -1,22 +1,39 @@
 package ru.otus.hw.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.otus.hw.exceptions.NotFoundException;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionHandlingController {
 
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public String methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex) {
+        log.error("Error: [{}]", ex.getMessage());
+        return "Error forming request";
+    }
+
     @ExceptionHandler(NotFoundException.class)
-    public String notFoundExceptionHandler() {
-        return "error/404";
+    @ResponseStatus(NOT_FOUND)
+    public String notFoundExceptionHandler(NotFoundException ex) {
+        log.error("Error: [{}]", ex.getMessage());
+        return "An error occurred while searching for the object";
     }
 
     @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
     public String exceptionHandler(Exception ex) {
         log.error("Error: [{}]", ex.getMessage());
-        return "error/500";
+        return "An unexpected error occurred";
     }
 }
--- hw10/src/main/java/ru/otus/hw/controllers/PageController.java
+++ hw10/src/main/java/ru/otus/hw/controllers/PageController.java
@@ -1,11 +1,13 @@
 package ru.otus.hw.controllers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class PageController {
