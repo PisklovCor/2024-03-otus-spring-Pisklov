22ac40e790b3adae032b6e9a7d5e8afcee62d16d
Andrey <andreypisklov@gmail.com>
2024-10-05 19:17:25 +0300

    [hw-14]: [ревью 2/2] добавление теста и профиля для отключения запуска ApplicationRunner в тестах

--- hw14/src/main/java/ru/otus/hw/cofiguration/ApplicationRunner.java
+++ hw14/src/main/java/ru/otus/hw/cofiguration/ApplicationRunner.java
@@ -2,9 +2,11 @@ package ru.otus.hw.cofiguration;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import ru.otus.hw.services.InstrumentService;
 
+@Profile("!test")
 @Component
 @RequiredArgsConstructor
 public class ApplicationRunner implements CommandLineRunner {
