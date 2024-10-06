9a71deaf1a9504d858558d27e0cb7c01fc5f8992
Andrey <andreypisklov@gmail.com>
2024-10-06 13:29:50 +0300

    [hw-14]: для хорошего тона добавил JsonIgnoreProperties

--- hw14/src/main/java/ru/otus/hw/domain/RawMessage.java
+++ hw14/src/main/java/ru/otus/hw/domain/RawMessage.java
@@ -1,5 +1,6 @@
 package ru.otus.hw.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RawMessage {
 
     @JsonProperty("instrument_type")
