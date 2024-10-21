647e22f943d9be34b2c990aaa897b7c1fc8a5225
Andrey <andreypisklov@gmail.com>
2024-10-21 18:06:56 +0300

    [hw-03]: переделаны тесты, удалены лишнии зависимости при использование spring-boot-starter-test, подпарвлены файлы описания проекта

--- hw03/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw03/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -4,28 +4,32 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.TestPropertySource;
 import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.domain.Question;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Сервис для работы с файловой системой ")
-@TestPropertySource("classpath:application_test.properties")
 class CsvQuestionDaoTest {
 
-//    @InjectMocks
-//    private AppProperties appProperties = new AppProperties("questions_test.csv", 3);
-//
-//    @DisplayName("должен проверить количество вопросов")
-//    @Test
-//    void findAll() {
-//        CsvQuestionDao csvQuestionDao = new CsvQuestionDao(appProperties);
-//        final List<Question> questionList = csvQuestionDao.findAll();
-//
-//        Assertions.assertEquals(5, questionList.size());
-//    }
+    @DisplayName("должен проверить количество вопросов")
+    @Test
+    void findAll() {
+        final Map<String, String> mockFile = new HashMap<>();
+        mockFile.put("ru-RU", "questions_test.csv");
+
+        final AppProperties properties = new AppProperties();
+        properties.setRightAnswersCountToPass(1);
+        properties.setLocale("ru-RU");
+        properties.setFileNameByLocaleTag(mockFile);
+
+        final CsvQuestionDao csvQuestionDao = new CsvQuestionDao(properties);
+        final List<Question> questionList = csvQuestionDao.findAll();
+
+        Assertions.assertEquals(5, questionList.size());
+    }
 }
--- hw03/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
+++ hw03/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -7,52 +7,47 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import ru.otus.hw.dao.CsvQuestionDao;
-import ru.otus.hw.dao.QuestionDao;
 import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
-@DisplayName("Сервис тестирования стендета ")
+@DisplayName("Сервис тестирования студента ")
 class TestServiceImplTest {
 
-//    private static final String STUDENT_FULL_NAME = "Student FullName";
-//
-//    @Mock
-//    private StreamsIOService ioService;
-//
-//    @Mock
-//    private CsvQuestionDao questionDao;
-//
-//    @InjectMocks
-//    private TestServiceImpl testService;
-//
-//    @DisplayName("должен вернуть корректный результат тестирования и имя")
-//    @Test
-//    void executeTestFor() {
-//
-//        List<Question> questionList = Collections.singletonList(new Question("test",
-//                Collections.singletonList(new Answer("text", true))));
-//        given(questionDao.findAll()).willReturn(questionList);
-//
-//        given(ioService.readIntForRangeWithPrompt(1, questionList.size(), "Select an answer option:",
-//                "A non-existent option was selected!"))
-//                .willReturn(1);
-//
-//        final Student student = new Student("Student", "FullName");
-//        var testResult = testService.executeTestFor(student);
-//
-//        Assertions.assertEquals(STUDENT_FULL_NAME, testResult.getStudent().getFullName());
-//        Assertions.assertEquals(1, testResult.getRightAnswersCount());
-//    }
+    private static final String STUDENT_FULL_NAME = "FirstName LastName";
+
+    @Mock
+    private LocalizedIOService ioService;
+
+    @Mock
+    private CsvQuestionDao questionDao;
+
+    @InjectMocks
+    private TestServiceImpl testService;
+
+    @DisplayName("должен вернуть корректный результат тестирования и имя")
+    @Test
+    void executeTestFor() {
+
+        List<Question> questionList = Collections.singletonList(new Question("test",
+                List.of(new Answer("text", true))));
+        given(questionDao.findAll()).willReturn(questionList);
+
+        given(ioService.readIntForRangeWithPromptLocalized(1, questionList.size(),
+                "TestService.choose.an.answer","TestService.error.message"))
+                .willReturn(1);
+
+        final Student student = new Student("FirstName", "LastName");
+        var testResult = testService.executeTestFor(student);
+
+        Assertions.assertEquals(STUDENT_FULL_NAME, testResult.getStudent().getFullName());
+        Assertions.assertEquals(1, testResult.getRightAnswersCount());
+    }
 }
