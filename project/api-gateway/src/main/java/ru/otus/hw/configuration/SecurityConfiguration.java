dc9cd29bc73ae7091abf7ec67f6073fe6b761d9b
Andrey <andreypisklov@gmail.com>
2024-12-05 19:36:04 +0300

    [project][api-gateway]: полная конфигурация keycloak и проброс токена в сервисы

--- project/api-gateway/src/main/java/ru/otus/hw/configuration/SecurityConfiguration.java
+++ project/api-gateway/src/main/java/ru/otus/hw/configuration/SecurityConfiguration.java
@@ -2,21 +2,22 @@ package ru.otus.hw.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http
-                .authorizeExchange(configurer -> configurer
-                        .pathMatchers("/**").authenticated())
-                .oauth2Login(Customizer.withDefaults())
-                .oauth2Client(Customizer.withDefaults())
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .build();
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(auth -> auth.anyExchange().authenticated())
+                .oauth2Login(withDefaults())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return http.build();
     }
 }
